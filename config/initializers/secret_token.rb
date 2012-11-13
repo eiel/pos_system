8ec53e3644377b692acd42317715aee63b6b140e
morishita <tomohiro.m0219@gmail.com>
2012-11-13 10:54:52 +0900

    街POSのRailsのセットアップ
    rails new により新規プロジェクト作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pos::Application.config.secret_token = 'dbf4761834a1a044f0ea4391c1c901e853d199d1a8e633fb3d4c854f0603257bcaf5682338f34cf681bd4aa3b46e1fac7ace4f4e6970b6e5debb21d44f05ac82'
