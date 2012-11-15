249590fb94e61f7f4e8c496e3577b1ed9d4ff7bd
morishita <tomohiro.m0219@gmail.com>
2012-11-15 15:18:22 +0900

    jsonテスト

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,5 +1,15 @@
+# -*- coding: utf-8 -*-
 class CustomerController < ApplicationController
   def index
+    @hoge = [{'menu'=>'和食','title'=>1},
+    	     {'menu'=>'洋食','title'=>2},
+    	     {'menu'=>'中華','title'=>3},
+    	     {'menu'=>'飲み物','title'=>4},
+    	     {'menu'=>'アルコール','title'=>4}]
     
+    respond_to do |format|
+      format.html
+      format.json { render :json => @hoge }
+    end
   end
 end
